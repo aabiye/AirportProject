beab26caaf8f3f01c9f131546b194cfb60f1b6b6
Abiye <abiyead@ad.vzwcorp.com>
2021-07-08 18:47:11 -0500

    UML diagram

---
+++ UML_AirportClassDiagram.js
@@ -0,0 +1,23 @@
+
+class Airport {
+	
+	name = String
+	location = String
+	ticketCounter()
+	security()
+	passengerCheckIn()
+	baggageCheckIn()
+	boardPlane()
+
+}
+
+class Passenger {
+
+	name = String
+	confimationNumber = Integer
+	passengerCheckIn()
+	baggageCheckIn()
+	boardPlane()
+	security()
+
+}
