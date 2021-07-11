e537f3bdca53b83dc6444b996551b4d12990566d
Abiye <aabiye7@gmail.com>
2021-07-10 19:35:26 -0500

    call back promise async

--- Airport.js
+++ Airport.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const fsp = require('fs').promises; 
+const path = require('path'); 
+
 class Airport {
 
     static Airports = []
@@ -15,6 +19,55 @@ class Airport {
     static addAirport(airport) {
         return airport
     }
+
+
+    getInfoCallback(callback){
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, 'airportsData.json')
+
+    
+        fs.readFile(dbPath, (err, data) => {
+            const airports = JSON.parse(data)
+            const airport = airports.find(airport => airport.iata === airportName)
+
+            callback(err, airport)
+        })
+    }
+
+    getInfoPromise() {
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, 'airportsData.json')
+
+        return new Promise((resolve, reject) => {
+            fs.readFile(dbPath, (err, data) => {
+                if(err) {
+                    return reject(err)
+                }
+                
+                const airports = JSON.parse(data)
+                const airport = airports.find(airport => airport.iata === airportName)
+
+                resolve(airport)
+            })
+        })
+    }
+
+    async getInfoAsync() {
+        const airportName = this.name;
+        const dbPath = path.join(__dirname, 'airportsData.json')
+        try {
+            const data = await fsp.readFile(dbPath)
+            const airports = JSON.parse(data)
+            const airport = airports.find(airport => airport.iata === airportName)
+        
+            return airport
+
+        } catch(err) {
+            console.log("AHH can't find airport info", err)
+        }
+      
+    }
+
 }
 
 const airport1 = new Airport('DFW')
--- Airport.test.js
+++ Airport.test.js
@@ -14,4 +14,24 @@ describe('Airport objects', () => {
         expect(airport1.planes.length).toBe(1);
     })
 
+    test('airport with call back', (done) => {
+        airport1.getInfoCallback((err, info) => {
+            console.log(info)
+            expect(info.country).toBe('US')
+            done()
+        })
+    })
+
+    test('getInfo with Promise', () => { 
+         return airport1.getInfoPromise()
+         .then(info => {
+          expect(info.country).toBe('US')
+         })
+     });
+ 
+     test('getInfo with Async Await', async () => { 
+         const info = await airport1.getInfoAsync()
+         expect(info.country).toBe('US')
+     });
+
 })
